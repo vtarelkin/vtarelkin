f63b16da042224fb252d36257d4182ce78bb3883
vtarelkin-comprehend <valery.tarelkin2@harman.com>
2019-01-20 00:17:09 +0300

    correct json parse

--- build.sbt
+++ build.sbt
@@ -9,5 +9,5 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-stream" % "2.5.13",
   "com.typesafe.akka" %% "akka-http" % "10.1.3",
   "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.3",
-  "io.spray" %%  "spray-json" % "1.3.4"
+   "io.spray" % "spray-json_2.12" % "1.3.3"
 )
--- src/main/scala/ApiRouter.scala
+++ src/main/scala/ApiRouter.scala
@@ -1,17 +1,23 @@
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{StatusCodes}
 import akka.http.scaladsl.server.Directives._
+import spray.json.{DefaultJsonProtocol, JsValue}
 
-object ApiRouter {
-  val route = getFromResourceDirectory("templates") ~ path("hello") {
-    get {
-      {
-        redirect("templates/index.html", StatusCodes.PermanentRedirect)
-      }
-    }~
-      post {
+object ApiRouter extends DefaultJsonProtocol {
+
+
+    val route = getFromResourceDirectory("templates") ~ path("hello") {
+      get {
         {
-          complete("Welcome to the system, username")
+          redirect("templates/index.html", StatusCodes.PermanentRedirect)
         }
-      }
+      } ~
+        post {
+          {
+            entity(as[JsValue]) {
+              jsonNameValue =>
+                complete(s"Welcome to the system, ${jsonNameValue.asJsObject.fields("name").convertTo[String]}")
+            }
+          }
+        }
+    }
   }
-}
